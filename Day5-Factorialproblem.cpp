39f67c3ad08b2356f5a5a5ec639b1c305d60c571
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2023-06-09 21:38:42 +0530

    Add files via upload

---
+++ Day5-Factorialproblem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int result = factorial(n);
+
+    cout << result << endl;
+
+    return 0;
+}
