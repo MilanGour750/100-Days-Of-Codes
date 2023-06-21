85d9585c1411f9c52395dc460995cccc2114559c
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2023-06-21 21:13:42 +0530

    Update Day17-countdivisor.cpp

--- Day17-countdivisor.cpp
+++ Day17-countdivisor.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int l, r, k;
+    cin >> l >> r >> k;
+    int count = 0;
+    for (int num = l; num <= r; num++) {
+        if (num % k == 0) {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
