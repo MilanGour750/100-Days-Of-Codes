44858b6e66175d68f6e4cce6682bcaf459f9d9d6
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2023-08-09 22:59:08 +0530

    Create Day66-CountCompleteTreeNodes.cpp

---
+++ Day66-CountCompleteTreeNodes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == nullptr){
+            return 0;
+        }
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
