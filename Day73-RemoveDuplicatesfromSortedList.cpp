8a66f813a0b87b5b8de237000c354d3a20b8e105
Milan Gour <90448124+MilanGour750@users.noreply.github.com>
2023-08-16 22:20:09 +0530

    Create Day73-RemoveDuplicatesfromSortedList.cpp

---
+++ Day73-RemoveDuplicatesfromSortedList.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* last = head;
+        ListNode* cur = head->next;
+        while(cur != NULL){
+            if(cur->val == last->val){
+                last->next = cur->next;
+            }else{
+                last = cur;
+            }
+            cur = cur->next;
+        }
+        return head;
+    }
+};
